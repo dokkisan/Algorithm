b8071b8326dfaded5c0a5a27c9b15aaeea9caf97
dokkisan <dokkisan.wang@gmail.com>
2023-02-15 15:07:38 +0900

    feat : 햄버거 만들기

---
+++ src/programmers/Hamburger.java
@@ -0,0 +1,27 @@
+package programmers;
+
+public class Hamburger {
+    public static void main(String[] args) {
+        int[] ingredient = {2, 1, 1, 2, 3, 1, 2, 3, 1};
+        System.out.println(Solution.solution(ingredient));
+    }
+
+    public static class Solution {
+        public static int solution(int[] ingredient) {
+            int answer = 0;
+
+            StringBuilder sb = new StringBuilder();
+            for (int i : ingredient) {
+                sb.append(i);
+
+                if (sb.length() > 3
+                        && sb.substring(sb.length() - 4, sb.length()).equals("1231")) {
+                    answer++;
+                    sb.delete(sb.length() - 4, sb.length());
+                }
+            }
+            return answer;
+
+        }
+    }
+}
